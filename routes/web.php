2ace5e6e8e94c5210579b49bbe2beae3b68b0df9
sanadajv1 <sanadajv@student.apc.edu.ph>
2023-12-28 13:22:59 +0800

    imports yearnart

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*Front Page*/
 Route::get('/', function () {
-    return view('welcome');
+    return view('YearnArt.Home');
 });
+
+Route::get('/About', function () {
+    return view('YearnArt.About');
+});
+
+require __DIR__.'/auth.php';
+
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
+
+require __DIR__.'/adminauth.php';
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::get('/FAQ', function () {
+    return view('YearnArt.FAQ');
+})->name('FAQ');
+
+Route::get('/Products', function () {
+    return view('YearnArt.Products');
+})->name('Products');
+
+Route::get('/About', function () {
+    return view('YearnArt.About');
+})->name('About');
+
+Route::get('/MyOrders', function () {
+    return view('YearnArt.MyOrders');
+})->name('MyOrders');
+
